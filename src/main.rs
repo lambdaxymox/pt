c87369bbd9dd3a50632646319dd2d95e3da81293
LambdaXymox <lambda.xymox@gmail.com>
2020-04-08 22:58:20 -0500

    Output generated image to a file.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
-fn main() {
+use std::fs::File;
+use std::io;
+use std::io::Write;
+
+
+fn main() -> io::Result<()> {
+    let mut file = File::create("output.ppm")?;
+
     let nx = 200;
     let ny = 100;
-    print!("P3\n{} \n255\n", nx);
+    write!(&mut file, "P3\n{} {}\n255\n", nx, ny).unwrap();
     for j in 0..(ny -1) {
         for i in 0..nx {
             let r = i as f32 / nx as f32;
@@ -10,7 +17,9 @@ fn main() {
             let ir = (255.99 * r) as u32;
             let ig = (255.99 * g) as u32;
             let ib = (255.99 * b) as u32;
-            print!("{} {} {}\n", ir, ig, ib);
+            write!(&mut file, "{} {} {}\n", ir, ig, ib).unwrap();
         }
     }
+
+    Ok(())
 }
